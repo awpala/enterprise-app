95542861ed162bcaccd46253f57b04d050cde079
awpala <adam@adam-pala.com>
2026-04-17 18:38:41 +0000

    fix: add migration to fix breaking pipeline

--- api/src/EA.Infrastructure/Migrations/20260417182133_ChangeAuditDetailsToJsonDocument.Designer.cs
+++ api/src/EA.Infrastructure/Migrations/20260417182133_ChangeAuditDetailsToJsonDocument.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using EA.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EA.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260417182133_ChangeAuditDetailsToJsonDocument")]
+    partial class ChangeAuditDetailsToJsonDocument
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
